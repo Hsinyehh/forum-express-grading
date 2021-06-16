30b3ae719dd548c968bc8a6bf65c9780052a43cb
Rita <rita.yeh@0617gmail.com>
2021-06-16 20:58:31 +0800

    feat: add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,32 @@ const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/', (req, res) => {
+
+  //前台
+  app.get('/', authenticated, (req, res) => {
     res.redirect('/restaurants')
   })
 
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   app.get('/admin', (req, res) => {
     res.redirect('/admin/restaurants')
   })
-  app.get('/admin/restaurants', adminController.getRestaurants)
+
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
@@ -21,4 +36,10 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
+
+  //後台
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+
+
 }
