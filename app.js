20c8a8a5ecea6fb38689e7f2f72580c43d255cb1
Rita <rita.yeh@0617gmail.com>
2021-06-16 20:27:02 +0800

    feat: add passport.js

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
-const session = require('express-session')
 const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.use(session({
   secret: 'secret',
   resave: false,
   saveUninitialized: false
 }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(flash())
 
@@ -29,6 +35,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
