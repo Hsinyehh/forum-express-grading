fd564d4b103663da64fc301b462a0f1d607d2c73
Rita <rita.yeh@0617gmail.com>
2021-06-29 22:57:51 +0800

    refact: DELETE /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -50,6 +50,16 @@ const categoryService = {
         })
     }
   },
+  deleteCategory: (req, res, callback) => {
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        category.destroy()
+          .then((category) => {
+            callback({ status: 'success', message: '' })
+          })
+      })
+  }
+
 
 }
 
