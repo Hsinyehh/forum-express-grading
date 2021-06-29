eaae3047408a9e636617cf18be4de82b08cfe009
Rita <rita.yeh@0617gmail.com>
2021-06-29 22:45:55 +0800

    refact:　PUT /api/admin/categories/:id

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -9,6 +9,9 @@ let categoryController = {
   postCategory: (req, res) => {
     categoryService.postCategory(req, res, (data) => { return res.json(data) })
   },
+  putCategory: (req, res) => {
+    categoryService.putCategory(req, res, (data) => { return res.json(data) })
+  },
 }
 
 
